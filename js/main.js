b593d58204d93f003fc95b7c48a9681075139bad
taku.kitano <kitaku95026@gmail.com>
2022-09-16 17:17:19 +0900

    JS最終確認前

---
+++ js/main.js
@@ -0,0 +1,123 @@
+// ハンバーガーメニュー================================================================
+window.onunload = function () { };
+function sizecheck() {
+  w = window.innerWidth ? window.innerWidth : $(window).width();
+  h = window.innerHeight ? window.innerHeight : $(window).height();
+}
+
+sizecheck();
+$(function () {
+  if (w > 769) {
+  } else {
+    $("header .drawer").click(function () {
+      $('body').toggleClass('nav-open');
+      $('header .header_nav').fadeToggle(200);
+    });
+    $("header li a").click(function () {
+      $('body').toggleClass('nav-open');
+      $('header .header_nav').fadeToggle(200);
+    });
+  }
+});
+// ===================================================================================
+
+// 下から上のアニメーション============================================================
+function fadeAnime() {
+  $('.fadeUpTrigger').each(function () {
+    var elemPos = $(this).offset().top - 30;//要素より、30px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeUp');
+    } else {
+      $(this).removeClass('fadeUp');
+    }
+  });
+}
+// 画面をスクロールをしたら動く
+$(window).scroll(function () {
+  fadeAnime();
+});
+
+// 順番に表示=========================================================================
+function delayScrollAnime() {
+  var time = 0.2;//遅延時間を増やす秒数の値
+  var value = time;
+  $('.delayScroll').each(function () {
+    var parent = this;          //親要素を取得
+    var elemPos = $(this).offset().top;//要素の位置まで来たら
+    var scroll = $(window).scrollTop();//スクロール値を取得
+    var windowHeight = $(window).height();//画面の高さを取得
+    var childs = $(this).children();  //子要素を取得
+
+    if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+      $(childs).each(function () {
+
+        if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+
+          $(parent).addClass("play"); //親要素にクラス名playを追加
+          $(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+          $(this).addClass("fadeUp");//アニメーションのクラス名を追加
+          value = value + time;//delay時間を増加させる
+
+          //全ての処理を終わったらplayを外す
+          var index = $(childs).index(this);
+          if ((childs.length - 1) == index) {
+            $(parent).removeClass("play");
+          }
+        }
+      })
+    } else {
+      $(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+      value = time;//delay初期値の数値に戻す
+    }
+  })
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+// ==================================================================================
+
+
+// ローディングアニメーション==========================================================
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定				
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform: 'translate(-50%,-50%)',
+      'font-size': '1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function (state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+  $(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+  $(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+  $("#splash").fadeOut();//#splashエリアをフェードアウト
+});
+// ===================================================================================
